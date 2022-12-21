0d491b868332ce718043eab366787a9367cd7462
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-21 13:48:38 +0530

    welcome

---
+++ fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logocalprogram
+{
+    internal class fibonacci
+    {
+        public static void fibbo()
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine("---ENTER NUMBER HERE---");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i < x; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+
+            }
+        }
+    }
+}
