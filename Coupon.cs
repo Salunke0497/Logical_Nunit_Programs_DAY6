fc37b886be1014bd9fbf1caf1502de17c9183bd3
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-12-22 17:24:53 +0530

    welcome

---
+++ Coupon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logocalprogram
+{
+    internal class Coupon
+    {
+        public static char[] Num = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static void coup()
+        {
+            Console.WriteLine("Enter N number of coupon: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int CLength = 6;
+
+            Random random = new Random();
+
+            for (int CNum = 1; CNum <= N; CNum++)
+            {
+                StringBuilder STRING = new StringBuilder();
+                for (int i = 0; i < CLength; i++)
+                {
+                    int position = random.Next(0, Num.Length);
+                    STRING.Append(Num[position]);
+                }
+             Console.WriteLine("Unique Coupon Number - {0}:  {1}", CNum, STRING);
+            }
+        }
+    }
+}
